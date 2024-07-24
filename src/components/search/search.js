ccb363a8bc7bc2ffc1d3387de696aa7e03279c3a
loafcheck <rachelskim.contact@gmail.com>
2024-07-24 16:14:55 -0700

    develop Search Component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,24 @@
-const Search = () => {
-    return ('Hello')
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = ({onSearchChange}) => {
+
+    const [search, setsearch] = useState(null);
+    const handleOnChange = (searchData) => {
+        setsearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate 
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+        />
+         
+  
+    )
 }
 
 export default Search;
