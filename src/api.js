e078969debc90d2217e798e35e7f71d808b85a28
loafcheck <rachelskim.contact@gmail.com>
2024-07-25 13:40:17 -0700

    Building city research component

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '6e57986e94mshf889c8707e126a8p1e11a7jsn8c7014d830a8',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,12 +1,46 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../api";
 
 const Search = ({onSearchChange}) => {
 
     const [search, setsearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch (
+            `${GEO_API_URL}/?namePrefix=${inputValue}`,
+        geoApiOptions
+        )
+
+        .then (response => {
+            return response.json();
+        })
+
+        .then(data => {
+            //Log the parsed JSON data
+            console.log('Full API response: ', data);
+            //Map the data to the required format
+            const options = data.data.map(city =>({
+                value: `${city.latitude} ${city.longitude}`, // Correct property names
+                label: `${city.name}, ${city.countryCode}` 
+            }));
+
+            console.log('Mapped Options: ', options);
+
+            return {options};
+        })
+
+        .catch(error => {
+            // Handle and log any errors
+            console.error('Error fetching data:', error);
+            // Return an empty options array in case of an error
+            return { options: [] };
+        });
+    };
+
     const handleOnChange = (searchData) => {
         setsearch(searchData);
-        onSearchChange(searchData);
+        onSearchChange(searchData); //Notify parent component
     }
 
     return (
@@ -15,6 +49,7 @@ const Search = ({onSearchChange}) => {
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
          
   
