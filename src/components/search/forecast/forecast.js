4be534cc026a58cb6cb06e75dc8816dc84305083
loafcheck <rachelskim.contact@gmail.com>
2024-08-19 14:08:47 -0700

    forecast application

--- src/components/search/forecast/forecast.js
+++ src/components/search/forecast/forecast.js
@@ -1,24 +1,47 @@
 import {Accordion, AccordionItemHeading, AccordionItem, AccordionItemPanel, AccordionItemButton} from "react-accessible-accordion";
+import './forecast.css';
+
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 const Forecast = ({data}) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.lenth).concat(WEEK_DAYS.slice(0,dayInAWeek));
+
+
     return (
     <>
         <label className="title">Daily Label</label>
+        
         <Accordion allowZeroExpanded>
             {data.list.splice(0,7).map((item, idx)=> (
                 <AccordionItem key={idx}>
                 <AccordionItemHeading>
                     <AccordionItemButton>
-                        Hello
+                        <div className="daily-item">
+                            <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`}/>
+                            <label className="day">{forecastDays[idx]}</label>
+                            <label className="description">{item.weather[0].description}</label>
+                            <label className="min-max">{Math.round(item.main.temp_min)}°C /{Math.round(item.main.temp_max)}°C</label>
+                        </div>
                     </AccordionItemButton>
                 </AccordionItemHeading>
-                <AccordionItemPanel></AccordionItemPanel>
+                <AccordionItemPanel>
+                    <div className="daily-details-grid">
+                        <div className="daily-details-grid-item">
+                            <label>Pressure</label>
+                            <label>{item.main.pressure}</label>
+                        </div>
+                    </div>
+                </AccordionItemPanel>
                 </AccordionItem>
             ))}
             
         </Accordion>
+        
     </>
+    
     )
+
 }
 
 export default Forecast;
