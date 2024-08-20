327360ee77e336fb061442523f5051be42215afa
loafcheck <rachelskim.contact@gmail.com>
2024-08-20 12:21:34 -0700

    forecast application design

--- src/components/search/forecast/forecast.js
+++ src/components/search/forecast/forecast.js
@@ -29,7 +29,27 @@ const Forecast = ({data}) => {
                     <div className="daily-details-grid">
                         <div className="daily-details-grid-item">
                             <label>Pressure</label>
-                            <label>{item.main.pressure}</label>
+                            <label>{item.main.pressure}hPa</label>
+                        </div>
+                        <div className="daily-details-grid-item">
+                            <label>Humidity</label>
+                            <label>{item.main.humidity}%</label>
+                        </div>
+                        <div className="daily-details-grid-item">
+                            <label>Clouds</label>
+                            <label>{item.clouds.all}%</label>
+                        </div>
+                        <div className="daily-details-grid-item">
+                            <label>Wind Speed</label>
+                            <label>{item.wind.speed}m/s</label>
+                        </div>
+                        <div className="daily-details-grid-item">
+                            <label>Sea Level</label>
+                            <label>{item.main.sea_level}m</label>
+                        </div>
+                        <div className="daily-details-grid-item">
+                            <label>Feels Like</label>
+                            <label>{Math.round(item.main.feels_like)}°C </label>
                         </div>
                     </div>
                 </AccordionItemPanel>
